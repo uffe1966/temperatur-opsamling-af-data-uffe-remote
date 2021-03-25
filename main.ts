b10f5d98787e9128f304788353c8176946289e47
uffe1966 <81055165+uffe1966@users.noreply.github.com>
2021-03-25 02:43:17 +0100

    Opdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(2000)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Sad)
+    basic.pause(2000)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Confused)
+    basic.pause(2000)
+})
 let Temperatur = 0
 basic.showString("Remote")
 radio.setGroup(55)
 basic.forever(function () {
     Temperatur = input.temperature()
-    radio.sendNumber(Temperatur)
     basic.showNumber(Temperatur)
+    radio.sendNumber(Temperatur)
     basic.pause(2000)
 })
